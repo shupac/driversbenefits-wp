899e7026a49f4c23f2a9236a184b1d2a799be1ca
Shu Liu <djshu.us@gmail.com>
2020-08-23 14:19:33 -0700

    Refactoring homepage to use blocks content

--- wp-content/themes/drivers-benefits/front-page.php
+++ wp-content/themes/drivers-benefits/front-page.php
@@ -1,23 +1,9 @@
 <?php get_header(); ?>
 	<div class="home__hero">
-		<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; endif; ?>
-	</div>
-
-<!-- 		<div class="home__cta">
-			<h3>
-				Free and Discounted Benefits
-			</h3>
-			<div class="home__cta_info">
-				for drivers of Black Car services such as <strong>Uber</strong>, <strong>Lyft</strong>, <strong>Via</strong>, <strong>Juno</strong>, and <strong>Vital</strong>
-			</div>
-			<button class="btn btn-primary">Use Your Benefits</button>
-			<div class="home__cta_signin">
-				Already enrolled? <a href="/verify">Sign In</a>
-			</div>
-		</div>
 		<div class="home__testimonials">
+
+		</div>
+<!-- 		<div class="home__testimonials">
 			<div class="home__slide">
 				<img src="<?php echo get_template_directory_uri(); ?>/assets/image-testimonial-1-bg.png">
 				<div class="home__testimonial">
@@ -34,7 +20,25 @@
 			<div class="home__slide">
 				<img src="<?php echo get_template_directory_uri(); ?>/assets/image-testimonial-1-bg.png">
 			</div>
+		</div> -->
+	</div>
+	<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+		<?php the_content(); ?>
+	<?php endwhile; endif; ?>
+
+<!-- 		<div class="home__cta">
+			<h3>
+				Free and Discounted Benefits
+			</h3>
+			<div class="home__cta_info">
+				for drivers of Black Car services such as <strong>Uber</strong>, <strong>Lyft</strong>, <strong>Via</strong>, <strong>Juno</strong>, and <strong>Vital</strong>
+			</div>
+			<button class="btn btn-primary">Use Your Benefits</button>
+			<div class="home__cta_signin">
+				Already enrolled? <a href="/verify">Sign In</a>
+			</div>
 		</div>
+
 	</div> -->
 
 
@@ -47,3 +51,47 @@
 <?php get_footer(); ?>
 
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+<script>
+	$('.home__cta').prependTo('.home__hero');
+	$('.home__cta_signin').appendTo('.home__cta');
+	$('.home__testimonial').each((index, el) => {
+		console.log(el);
+		const slide = $.parseHTML(`
+			<div class="home__slide">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/image-testimonial-1-bg.png">
+				<div class="home__testimonial">
+					<div>
+						"Having these benefits gave me and my family peace of mind."
+					</div>
+					<div class="home__testimonial_person">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icon-uber.png">
+						Gupta Aditi
+					</div>
+					<img class="home__testimonial_avatar" src="<?php echo get_template_directory_uri(); ?>/assets/image-testimonial-1-avatar.png">
+				</div>
+			</div>
+			`
+		)
+
+		$(slide).appendTo($('.home__testimonials'))
+
+
+		// ('.home__hero');
+	});
+
+	$('.home__testimonials').slick({
+		dots: true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+	});
+
+	setTimeout(() => {
+		$('.slick-dots').appendTo($('.home__hero'))
+		$('.home__hero').fadeTo(100, 1);
+	}, 100);
+</script>
+
